5631fc7e725071b2057ca890a3f3172a941e5d7f
THANGARAJ <112554409+THANGARAJ-GLIT@users.noreply.github.com>
2022-09-16 11:37:16 +0530

    Add files via upload

---
+++ perfect number.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main() {
+   int reminder, sum=0, i, originalNum;
+    printf(" enter number: ");
+    scanf("%d",&originalNum);
+    for (i = 1; i <= originalNum/2; i++)
+        {
+            reminder = originalNum % i;
+	        if (reminder == 0)
+            {
+                sum = sum + i;
+            }
+        }
+        if (sum == originalNum)
+            printf("given no. is perfect number");
+        else
+            printf("given no. is not a perfect number");
+    getch();
+}
